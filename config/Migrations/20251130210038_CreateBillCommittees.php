1d84d5737dbced38b2b388f10e07b21f28b49d2a
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-11-30 14:02:23 -0700

    Added BillCommittees migrations

---
+++ config/Migrations/20251130210038_CreateBillCommittees.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class CreateBillCommittees extends BaseMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('bill_committees')
+            ->addColumn('bill_id', 'integer', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('committee_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('chamber', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('chamber_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime')
+            ->addColumn('modified', 'datetime')
+            ->create();
+    }
+}
