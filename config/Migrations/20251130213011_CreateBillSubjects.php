1940560b927a7b7e4711c325d7c78b3d92c0f4f7
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-11-30 14:31:25 -0700

    Added BillSubjects migration

---
+++ config/Migrations/20251130213011_CreateBillSubjects.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class CreateBillSubjects extends BaseMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('bill_subjects')
+            ->addColumn('bill_id', 'integer', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('subject_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('subject_name', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime')
+            ->addColumn('modified', 'datetime')
+            ->create();
+    }
+}
