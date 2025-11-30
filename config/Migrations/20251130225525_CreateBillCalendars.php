043f10c9972f179a729e3f2d2a71235d68e82ce5
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-11-30 16:05:54 -0700

    Added BillCalendars migration

---
+++ config/Migrations/20251130225525_CreateBillCalendars.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class CreateBillCalendars extends BaseMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('bill_calendars')
+            ->addColumn('bill_id', 'integer', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('type_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('type', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('date', 'date', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('time', 'time', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('location', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('description', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime')
+            ->addColumn('modified', 'datetime')
+            ->create();
+    }
+}
