cffb1f6fcaf2129b7655d40fca3537a9a2fed5d8
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-11-30 11:34:18 -0700

    Added SessionListRecords migraiton

--- config/Migrations/20251130182358_CreateSessionListRecords.php
+++ config/Migrations/20251130182358_CreateSessionListRecords.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use Migrations\BaseMigration;
+use Migrations\Db\Adapter\MysqlAdapter;
 
 class CreateSessionListRecords extends BaseMigration
 {
@@ -15,7 +16,78 @@ class CreateSessionListRecords extends BaseMigration
      */
     public function change(): void
     {
-        $table = $this->table('session_list_records');
-        $table->create();
+        $this
+            ->table('session_list_records')
+            ->addColumn('session_id', 'integer', [
+                'default' => null,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addColumn('state_id', 'integer', [
+                'default' => null,
+                'null' => false,
+                'signed' => false,
+                'limit' => MysqlAdapter::INT_TINY,
+            ])
+            ->addColumn('state_abbr', 'string', [
+                'default' => null,
+                'null' => false,
+                'limit' => 2,
+            ])
+            ->addColumn('year_start', 'date', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('year_end', 'date', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('prefile', 'integer', [
+                'default' => null,
+                'null' => false,
+                'limit' => MysqlAdapter::INT_TINY,
+            ])
+            ->addColumn('sine_die', 'integer', [
+                'default' => null,
+                'null' => false,
+                'limit' => MysqlAdapter::INT_TINY,
+            ])
+            ->addColumn('prior', 'integer', [
+                'default' => null,
+                'null' => false,
+                'limit' => MysqlAdapter::INT_TINY,
+            ])
+            ->addColumn('special', 'integer', [
+                'default' => null,
+                'null' => false,
+                'limit' => MysqlAdapter::INT_TINY,
+            ])
+            ->addColumn('session_tag', 'string', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('session_title', 'string', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('session_name', 'string', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('dataset_hash', 'string', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('session_hash', 'string', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime')
+            ->addColumn('modified', 'datetime')
+            ->create();
     }
 }
