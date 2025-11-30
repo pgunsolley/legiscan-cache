2bda94a9992d0df45f4ffed95885f502b318a976
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-11-30 14:06:04 -0700

    Added BillReferrals migration

---
+++ config/Migrations/20251130210248_CreateBillReferrals.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class CreateBillReferrals extends BaseMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('bill_referrals')
+            ->addColumn('bill_id', 'integer', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('date', 'date', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('committee_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('chamber', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('chamber_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime')
+            ->addColumn('modified', 'datetime')
+            ->create();
+    }
+}
