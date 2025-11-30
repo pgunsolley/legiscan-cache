d99856adedc9c61166f52799200cfbe7f8cc2c3b
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2025-11-30 15:54:53 -0700

    Added BillSupplements migration

---
+++ config/Migrations/20251130224641_CreateBillSupplements.php
@@ -0,0 +1,80 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class CreateBillSupplements extends BaseMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/migrations/4/en/migrations.html#the-change-method
+     *
+     * @return void
+     */
+    public function change(): void
+    {
+        $this
+            ->table('bill_supplements')
+            ->addColumn('bill_id', 'integer', [
+                'default' => null,
+                'null' => false,
+            ])
+            ->addColumn('supplement_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('date', 'date', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('type', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('type_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('description', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('mime', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('mime_id', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('url', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('state_link', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('supplement_size', 'integer', [
+                'default' => null,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('supplement_hash', 'string', [
+                'default' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime')
+            ->addColumn('modified', 'datetime')
+            ->create();
+    }
+}
