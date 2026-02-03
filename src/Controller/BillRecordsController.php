67af7a30376d25419ab8c80d85f270815354f702
Patrick Gunsolley <patrick.gunsolley@outlook.com>
2026-02-03 15:13:38 -0700

    Refactor BillRecordsController

--- src/Controller/BillRecordsController.php
+++ src/Controller/BillRecordsController.php
@@ -5,9 +5,6 @@ namespace App\Controller;
 
 use App\Service\DataSync\EntityChecker;
 use App\Service\DataSyncService;
-use Cake\Http\Exception\BadRequestException;
-use Cake\ORM\Association;
-use Cake\Utility\Inflector;
 
 /**
  * BillRecords Controller
@@ -16,63 +13,23 @@ use Cake\Utility\Inflector;
  */
 class BillRecordsController extends AppController
 {
-    public function view(DataSyncService $dataSyncService)
+    public function initialize(): void
     {
-        $billId = (int)$this->request->getQuery('billId');
-        if (empty($billId)) {
-            throw new BadRequestException('Missing required query: billId');
-        }
+        parent::initialize();
+        $this->loadComponent('Pick');
+    }
 
+    public function view(DataSyncService $dataSyncService, int $billId)
+    {
         if (!$this->BillRecords->existsForBillId($billId)) {
             $dataSyncService->syncBill($billId, new EntityChecker());
         }
 
-        $pickedColumns = $this->getRequest()->getQuery('pick');
-        if (!empty($pickedColumns)) {
-            $this->BillRecords->pick($pickedColumns);
-        }
-
-        $data = $this->BillRecords->find('byBillId', billId: $billId)->first();
-        $this->viewBuilder()->setOption('serialize', 'data');
-        $this->set(compact('data'));
-    }
-
-    public function getAssociation(string $associationName)
-    {
-        $billRecordId = (int)$this->request->getQuery('billRecordId');
-        if (empty($billRecordId)) {
-            throw new BadRequestException('Missing required query: billRecordId');
-        }
-
-        $association = $this
+        $data = $this
             ->BillRecords
-            ->getAssociation(Inflector::pluralize(Inflector::classify(Inflector::underscore($associationName))));
-
-        $pickedColumns = $this->getRequest()->getQuery('pick');
-        if (!empty($pickedColumns)) {
-            $association->pick($pickedColumns);
-        }
-
-        $data = $association
-            ->find()
-            ->where(['bill_record_id' => $billRecordId]);
-
-        if (in_array($association->type(), [Association::ONE_TO_ONE, Association::MANY_TO_ONE])) {
-            $data = $data->first();
-        } else {
-            $data = $this->paginate($data);
-            $this->set('pagination', [
-                'page_count' => $data->pageCount(),
-                'current_page' => $data->currentPage(),
-                'has_next_page' => $data->hasNextPage(),
-                'has_prev_page' => $data->hasPrevPage(),
-                'count' => $data->count(),
-                'total_count' => $data->totalCount(),
-                'per_page' => $data->perPage(),
-            ]);
-        }
-
-        $this->viewBuilder()->setOption('serialize', ['data', 'pagination']);
+            ->find('byBillId', billId: $billId)
+            ->contain(['BillRecordCommittees', 'BillRecordSessions']);
+        $this->viewBuilder()->setOption('serialize', 'data');
         $this->set(compact('data'));
     }
 }
